bac3a3f8bd38ea4e89e0b4951d140b9326682d8a
ttd1026 <daothanhtung94@gmail.com>
2018-05-09 17:39:00 +0700

    Commite end of day

--- chapter12.R
+++ chapter12.R
@@ -105,3 +105,69 @@ tibble(x = c("a,b,c", "d,e", "h,i,j")) %>%
 
 table5 %>% 
   unite(new, "century", "year", sep = "", remove = TRUE)
+
+#2
+table5_united <- table5 %>% 
+  unite("new", "century", "year", sep = "", remove = TRUE)
+
+#3
+table5_united %>% 
+  separate("rate", into = c("cases", "population"), sep = "/")
+
+# Missing values
+stocks <- tibble(
+  year   = c(2015, 2015, 2015, 2015, 2016, 2016, 2016),
+  qtr    = c(   1,    2,    3,    4,    2,    3,    4),
+  return = c(1.88, 0.59, 0.35,   NA, 0.92, 0.17, 2.66)
+)
+
+stocks %>% 
+  spread(year, return) %>% 
+  gather(year, return, "2015":"2016", na.rm = FALSE)
+
+stocks %>% 
+  complete(year, qtr)
+
+treatment <- tribble(
+  ~ person,           ~ treatment, ~response,
+  "Derrick Whitmore", 1,           7,
+  NA,                 2,           10,
+  NA,                 3,           9,
+  "Katherine Burke",  1,           4
+)
+
+treatment %>% 
+  fill(person)
+
+#Case study
+who1 <- who %>% 
+  gather(new_sp_m014:newrel_f65, key = "key", value = "cases", na.rm = TRUE)
+
+who1 %>% 
+  count(key)
+who2 <- who1 %>% 
+  mutate(key = stringr::str_replace(key, "newrel", "new_rel"))
+
+who3 <- who2 %>% #splitting key into relevant variables
+  separate(key, into = c("new", "type", "sexage"), sep = "_")
+
+who4 <- who3 %>% 
+  select(-new, -iso2, -iso3)
+
+who5 <- who4 %>% 
+  separate(sexage, into = c("sex", "age"), sep = 1)
+
+who_tidy <- who %>% 
+  gather(new_sp_m014:newrel_f65, key = "key", value = "cases", na.rm = TRUE) %>% 
+  mutate(key = stringr::str_replace(key, "newrel", "new_rel")) %>% 
+  separate(key, into = c("new", "type", "sexage"), sep = "_") %>% 
+  select(-iso2, -iso3, -new) %>% 
+  separate(sexage, into = c("sex", "age"), sep = 1)
+
+#Exercise 4
+who_tidy %>% 
+  group_by(country, year, sex) %>% 
+  summarize(cases = sum(cases)) %>% 
+  unite(country_sex, country, sex, remove = FALSE) %>% 
+  ggplot(mapping = aes(year, cases, group = country_sex, color = sex)) +
+  geom_line()
