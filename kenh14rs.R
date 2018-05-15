173be74182bbad72d513ffbdee88ebeed701f2eb
Tung Dao <daothanhtung94@gmail.com>
2018-05-15 23:59:47 +0700

    Plotting kenh14

--- kenh14rs.R
+++ kenh14rs.R
@@ -7,21 +7,24 @@ json_file <- fromJSON("kenh14data.json")
 # Tidying data
 df <- as.tibble(json_file) %>% 
   unnest(list) %>% 
-  mutate(date = parse_date(day)) %>% 
+  mutate(date = parse_date(day), 
+         box_id = parse_character(box_id)) %>%
+  arrange(as.integer(box_id))
   select(-size, -day)
 
-kenh14_df <- df[c(6, 1, 2, 3, 4, 5)]
+df <- df[c(6, 1, 2, 3, 4, 5)]
 
-kenh14_df %>% gather(metrics, c(view_total, view_scroll, user_total, user_scroll))
-
-total_view_df <- kenh14_df %>% 
-  group_by(date) %>% 
+kenh14_df <- df %>% 
+  group_by(date, box_id) %>% 
   summarize(view_total = sum(view_total), view_scroll = sum(view_scroll), 
-            sum(user_total), sum(user_scroll)) %>% 
-  select(date, view_total, view_scroll)
+            sum(user_total), sum(user_scroll))
 
 #Visualize
-ggplot(total_view_df, aes(x = date, y = view_scroll)) +
-  geom_bar(aes(fill = view_total), stat = "identity", position = "dodge")
+ggplot(kenh14_df, aes(date, view_scroll, group = box_id, color = box_id)) +
+  scale_x_date(date_breaks = "2 days",
+               date_labels = "%B %d") +
+  scale_y_continuous(labels = comma) +
+  geom_line(size = 1) +
+  ggtitle("view_scroll by box_id") +
+  theme_minimal()
 
-melt(total_view_df)
