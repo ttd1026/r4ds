35be94f40cd5dd6921bc03ccfecbc140e96e1ca0
ttd1026 <daothanhtung94@gmail.com>
2018-05-15 16:55:38 +0700

    chapter14

--- chapter14.R
+++ chapter14.R
@@ -44,3 +44,55 @@ str_view("\"'\\", "\"'\\\\")
 str_view(c("$^$", "abc$^$abc"), 
          "$\\^\\$")
 
+#Exercises
+#words starting with vowel
+str_view(words, "^[aeiou].", match = TRUE)
+#fruits starting with consonants
+str_view(fruit, "^[^aeiou].", match = TRUE)
+# ending with "ed" not "eed"
+str_view(c(words, "ed"), "^ed$|[^e]ed$", match = TRUE)
+
+str_view(words, ".q.|q.", match = TRUE)
+
+#Repitition
+x <- "1888 is the longest year in Roman numerals: MDCCCLXXXVIII"
+str_view(x, "CC?")
+
+#Grouping and backreference
+str_view(fruit, "(..)\\1", match = TRUE)
+
+no_vowels_1 <- !str_detect(words, "[aeiou]")
+no_vowels_2 <- str_detect(words, "^[^aeiou]+$")
+identical(no_vowels_1, no_vowels_2)
+
+words[str_detect(words, "x$")]
+str_subset(words, "x$")
+
+df <- tibble(
+  word = words,
+  i = seq_along(word)
+)
+
+df %>% 
+  filter(str_detect(words, "x$"))
+
+words[str_detect(words, "^x|x$")]
+words[str_detect(words, "^[aeiou].*[^aeiou]$")] %>% head()
+
+prop_vowels <- str_count(words, "[aeiou]") / str_length(words)
+max(prop_vowels)
+
+# Extract matches
+colors <- c("red", "orange", "yellow", "green", "blue", "purple")
+color_match <- str_c(colors, collapse = "|") #create color vector
+
+has_color <- str_subset(sentences, color_match)
+head(has_color)
+matches <- str_extract(has_color, color_match)
+head(matches)
+
+more <- sentences[str_count(sentences, color_match) > 1]
+str_view_all(more, color_match)
+str_extract_all(more, color_match)
+
+str_extract_all(more, color_match, simplify = TRUE)
